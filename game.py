09795ec2315b560bc04ab71ad2d886802b610b52
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-08-16 13:47:55 -0700

    finish WPM stuff

--- game.py
+++ game.py
@@ -1,17 +1,24 @@
 
+import time
+
 str_list = ["Paragraphs are the building blocks of papers.",
             "Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc.",
             "In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph."]
 
 
-def is_wrong(user_input, string):
+def is_correct(user_input, string):
     """checks if the string typed in is correct"""
     if(user_input == string):
         return True
     else:
         return False
 
-# def calculate_WPM()
+def calculate_WPM(num_errors, typed_length, minutes):
+    """Calculate the net WPM"""
+    words = (typed_length/5) - num_errors
+    if words > 0:
+        return words/minutes
+    return 0
 
 def start_word_game(sentence_list):
     """Start the game"""
@@ -20,27 +27,39 @@ def start_word_game(sentence_list):
     output = ''
     sum = 0
     error = 0
+    len_str_list = 0
+    start_time = time.time()
     while game_running:
         user_input = input('\n' + sentence_list[i] + '\n\nType the sentence shown above!\n')
-        
-        if is_wrong(user_input, sentence_list[i]):
+        index = 0
+        len_str_list += len(user_input)
+
+        if is_correct(user_input, sentence_list[i]):
             print('Correct\n')
         else:
             print('Incorrect\n')
-            # for char_index in range(0, len(sentence_list[i])):
-            #     if()
-            #     if(user_input[char_index] != sentence_list[i]):
-            #         error += 1
+            for user_char, orig_char in zip(user_input, sentence_list[i]):
+                if user_char != orig_char:
+                    error += 1
+                index += 1
+            for sen_index in range(index, len(sentence_list[i])):
+                error += 1
+            index = 0
         
         i += 1
         valid_input = False
 
         if(i == len(sentence_list)):
+            end_time = time.time()
+            elasped_time_min = (end_time - start_time)/60
+
+            WPM = calculate_WPM(error, len(user_input), elasped_time_min)
             if(error == 1):
-                print('\n You have {} error'.format(error))
+                print('\nYou have completed {} characters with {} error in {} minutes'.format(len(user_input), error, elasped_time_min))
             else:
-                print('\nYou have {} errors'.format(error))
-        
+                print('\nYou have completed {} characters with {} errors in {} minutes'.format(len(user_input), error, elasped_time_min))
+            print('Your WPM is {}\n'.format(WPM))
+
             while not valid_input:
                 input_lower = input('[R]eset the game    [M]enu to return menu    [E]xit the game\n').lower()
                 if input_lower == 'r' or input_lower == 'reset':
