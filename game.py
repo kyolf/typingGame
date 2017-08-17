f0f43ea3e16113cfe70bb45d49e14f7b610faefc
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-08-17 12:06:15 -0700

    changed some sentences

--- game.py
+++ game.py
@@ -18,7 +18,7 @@ medium1 = ["Paragraphs are the building blocks of papers.",
         "Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc.",
         "In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph."]
 
-medium2 = ["This quote is in the article, Programming's Dirtiest Little Secret, by Steve Yegge.",
+medium2 = ["Steve Yegge says this in his blog, Programming's Dirtiest Little Secret.",
         "\"I can't understand why professional programmers out there allow themselves to have a career without teaching themselves to type.",
         "It doesn't make any sense. It's like being, I dunno, an actor without knowing how to put your clothes on.\""]
 
