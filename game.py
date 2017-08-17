5c937742683fc9d9f7ec6cf32b0ccb0ee31ea7fd
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-08-17 03:40:12 -0700

    got random easy, medium, hard list showing

--- game.py
+++ game.py
@@ -1,17 +1,60 @@
 
 import time
-easy_list = ["Congratulations, you have selected the easy difficulty!",
-            "This should not be hard to type.",
-            "This should be easy to type"]
+import random
 
-medium_list = ["Paragraphs are the building blocks of papers.",
-            "Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc.",
-            "In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph."]
+easy1 = ["Congratulations, you have selected the easy difficulty!",
+        "This should not be hard to type.",
+        "This should be easy to type"]
 
-hard_list = ["According to Martin Krzywinski, a Canadian specialist in bioinformatics, these are some of the " + 
-            "hardest words to type on a standard QWERTY keyboard: pizazz, piazzas, pizzas, suburban, assuming, obstinance, foramens.",
-            "The difficulty of these words was determined by the physical effort used to type them.",
-            "Doubled letters can be hard, especially when the letter is typed with one of the little."]
+easy2 = ["I love to go to sleep!",
+        "What about you?",
+        "Sleep is awesome!!!"]
+
+easy3 = ["The club isn't the best place to find a lover",
+        "So the bar is where I go",
+        "Me and my friends at the table doing shots"]
+
+medium1 = ["Paragraphs are the building blocks of papers.",
+        "Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc.",
+        "In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph."]
+
+medium2 = ["This quote is in the article, Programming's Dirtiest Little Secret, by Steve Yegge.",
+        "\"I can't understand why professional programmers out there allow themselves to have a career without teaching themselves to type.",
+        "It doesn't make any sense. It's like being, I dunno, an actor without knowing how to put your clothes on.\""]
+
+medium3 = ["\"Tests are a good thing. The knowledge gained from a night of cramming will be all but forgotten once you hit adulthood.",
+        "And that's fine. The experience you gain through competition, having sharpened your skills and broadened " +
+        "your minds all under the same set of rules? That's the real treasure.\"",
+        "Assassination Classroom, by Korosensei"]
+
+hard1 = ["According to Martin Krzywinski, a Canadian specialist in bioinformatics, these are some of the " + 
+        "hardest words to type on a standard QWERTY keyboard: pizazz, piazzas, pizzas, suburban, assuming, obstinance, foramens.",
+        "The difficulty of these words was determined by the physical effort used to type them.",
+        "Doubled letters can be hard, especially when the letter is typed with one of the little."]
+
+hard2 = ['As you can see in the title this is "A very hard typing test text." ' +
+        "Someone must have the worldwide record as at least 60 seconds to he put up on my typing test wall! " +
+        "I will be checking this page every day to see if someone got this score.",
+        "Most likely I will! Me! It's very fun making these types of elaborate things " +
+        "because you can write whatever you want! And input commonly mispeled words!",
+        "As you probably noticed by now, that mistake was on purpose. Or was it? " + 
+        "Do your best! I will do my best. Believe in yourself. This thing is gonna repeat for hours. And hours."]
+
+hard3 = ["A plasma display panel (PDP) is a type of flat panel display now commonly " +
+        "used for large TV displays (typically above 37-inch or 940 mm).",
+        "Many tiny cells located between two panels of glass hold an inert mixture of noble gases (neon and xenon).",
+        "The gas in the cells is electrically turned into a plasma which then excites phosphors to emit light. " +
+        "The display panel is only about 6 cm (2.5 inches) thick, while the total " + 
+        "thickness, including electronics, is less than 10 cm (4 inches)."]
+
+easy_list = [easy1, easy2, easy3]
+medium_list = [medium1, medium2, medium3]
+hard_list = [hard1, hard2, hard3]
+
+def rand_sentence_list(sentence_list):
+    rand_index = random.randint(0, len(sentence_list) - 1)
+    print('this is random {}'.format(rand_index))
+    return sentence_list[rand_index]
 
 def is_correct(user_input, string):
     """checks if the string typed in is correct"""
@@ -131,13 +174,13 @@ if __name__ == "__main__":
 
         #Start Game
         elif user_input == 'ea' or user_input == 'easy':
-            user_input = start_word_game(easy_list)
+            user_input = start_word_game(rand_sentence_list(easy_list))
 
         elif user_input == 'med' or user_input == 'medium':
-            user_input = start_word_game(medium_list)
+            user_input = start_word_game(rand_sentence_list(medium_list))
 
         elif user_input == 'h' or user_input == 'hard':
-            user_input = start_word_game(hard_list)
+            user_input = start_word_game(rand_sentence_list(hard_list))
         
         #End Game
         elif user_input == 'e' or user_input == 'exit':
