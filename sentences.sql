e680f08d43830935c8badd55341576c14de6844a
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-08-17 14:43:40 -0700

    fixed the insert into by changing \' to ''

--- sentences.sql
+++ sentences.sql
@@ -5,7 +5,7 @@ VALUES ('Congratulations, you have selected the easy difficulty!',
        ('I love to go to sleep!',
        'What about you?',
        'Sleep is awesome!!!'),
-       ('The club isn\'t the best place to find a lover',
+       ('The club isn''t the best place to find a lover',
        'So the bar is where I go',
        'Me and my friends at the table doing shots');
 
@@ -13,11 +13,11 @@ INSERT INTO medium_list(sentence1, sentence2, sentence3)
 VALUES ('Paragraphs are the building blocks of papers.',
         'Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc.',
         'In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph.'),
-        ('Steve Yegge says this in his blog, Programming\'s Dirtiest Little Secret.',
-        '"I can\'t understand why professional programmers out there allow themselves to have a career without teaching themselves to type.',
-        'It doesn\'t make any sense. It\'s like being, I dunno, an actor without knowing how to put your clothes on."'),
-        ('\"Tests are a good thing. The knowledge gained from a night of cramming will be all but forgotten once you hit adulthood.',
-        'And that\'s fine. The experience you gain through competition, having sharpened your skills and broadened your minds all under the same set of rules? That\'s the real treasure."',
+        ('Steve Yegge says this in his blog, Programming''s Dirtiest Little Secret.',
+        '"I can''t understand why professional programmers out there allow themselves to have a career without teaching themselves to type.',
+        'It doesn''t make any sense. It''s like being, I dunno, an actor without knowing how to put your clothes on."'),
+        ('"Tests are a good thing. The knowledge gained from a night of cramming will be all but forgotten once you hit adulthood.',
+        'And that''s fine. The experience you gain through competition, having sharpened your skills and broadened your minds all under the same set of rules? That''s the real treasure."',
         'Assassination Classroom, by Korosensei');
 
 INSERT INTO hard_list(sentence1, sentence2, sentence3)
@@ -25,7 +25,7 @@ VALUES ('According to Martin Krzywinski, a Canadian specialist in bioinformatics
         'The difficulty of these words was determined by the physical effort used to type them.',
         'Doubled letters can be hard, especially when the letter is typed with one of the little.'),
         ('As you can see in the title this is "A very hard typing test text." Someone must have the worldwide record as at least 60 seconds to he put up on my typing test wall! I will be checking this page every day to see if someone got this score.',
-        'Most likely I will! Me! It\'s very fun making these types of elaborate things because you can write whatever you want! And input commonly mispeled words!',
+        'Most likely I will! Me! It''s very fun making these types of elaborate things because you can write whatever you want! And input commonly mispeled words!',
         'As you probably noticed by now, that mistake was on purpose. Or was it? Do your best! I will do my best. Believe in yourself. This thing is gonna repeat for hours. And hours.'),
         ('A plasma display panel (PDP) is a type of flat panel display now commonly used for large TV displays (typically above 37-inch or 940 mm).',
         'Many tiny cells located between two panels of glass hold an inert mixture of noble gases (neon and xenon).',
